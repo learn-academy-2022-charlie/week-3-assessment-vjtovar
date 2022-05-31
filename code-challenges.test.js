b778e5992214ebce93224697b5921d919b5863b0
vjtovar <vjtovar25@gmail.com>
2022-05-31 08:11:34 -0700

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,47 +10,131 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-
+// a describe method that lists the name of the function OR naming of the particular test.
+// a test/it method, nested within the describe block, that in plain words, describes that the function does.
+//an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      
+  
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
-
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+describe("lengthArr", () => {
+    it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+    const fibLength1 = 6
+     // Expected output: [1, 1, 2, 3, 5, 8]
+    const fibLength2 = 10
+    // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        expect(lengthArr(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+        expect(lengthArr(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
+// FAIL  ./code-challenges.test.js
+// lengthArr
+//   ✕ takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence
+// ● lengthArr › takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence
+//   ReferenceError: lengthArr is not defined
 
 
 // b) Create the function that makes the test pass.
+// Declare a function named lengthArr
+// Assign array as parameter
+// Assign variables fib and newArray
+// Use for loop to get previous numbers 
+// Return array the length of number with Fibonacci sequence
+
+const lengthArr = (array) =>{
+let fib = [1, 0]
+let newArray = []
+for(let i=2; i <= array + 1; i++) {
+    fib[i] = fib[i-2] + fib[i-1]
+    newArray.push(fib[i])
+}
+return newArray
+}
+// PASS  ./code-challenges.test.js
+// lengthArr
+//   ✓ takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence (1 ms)
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
-
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
+describe("oddNum", () => {
+    it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+        const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+         // Expected output: [-9, 7, 9, 199]
+        const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+        // Expected output: [-823, 7, 23]
+        expect(oddNum(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(oddNum(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
+// FAIL  ./code-challenges.test.js
+// oddNum
+//   ✕ takes in an array and returns a new array of only odd numbers sorted from least to greatest (1 ms)
+// ● oddNum › takes in an array and returns a new array of only odd numbers sorted from least to greatest
+//   ReferenceError: oddNum is not defined
 
 
 // b) Create the function that makes the test pass.
+// Declare a function named oddNum
+// Assign array as parameter
+// Use filter method to iterate through the array 
+// Return new array with only odd numbers and sorted least to greatest
+
+const oddNum = (array) => {
+    let newArray = array.filter(value => {
+        return value % 2 !== 0 && typeof value !== "string" && value !== null && value !== true && value !== false
+        })
+        return newArray.sort((a, b) => a - b)
+    } 
+//     PASS  ./code-challenges.test.js
+//     oddNum
+//       ✓ takes in an array and returns a new array of only odd numbers sorted from least to greatest (1 ms)
+  
+//   Test Suites: 1 passed, 1 total
+//   Tests:       1 passed, 1 total
+
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("sumArray", () => {
+    it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array", () => {
+        const numbersToAdd1 = [2, 4, 45, 9]
+        // Excpected output: [2, 6, 51, 60]
+        const numbersToAdd2 = [0, 7, -8, 12]
+       // Expected output: [0, 7, -1, 11]
+        const numbersToAdd3 = []
+        // Expected output: []
+        expect(sumArray(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(sumArray(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(sumArray(numbersToAdd3)).toEqual([])
+    })
+})
+// FAIL  ./code-challenges.test.js
+// sumArray
+//   ✕ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array (1 ms)
+// ● sumArray › takes in an array and returns an array of the accumulating sum. An empty array should return an empty array
+//   ReferenceError: sumArray is not defined
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-
-const numbersToAdd3 = []
-// Expected output: []
 
 
 // b) Create the function that makes the test pass.
+// Declare a function named sumArray
+// Assign array as parameter
+// Use map method to iterate through the array 
+// Return array with accumulating sum
+
+const sumArray = (array) => array.map((sum => value => sum += value)(0))
+
+// PASS  ./code-challenges.test.js
+// sumArray
+//   ✓ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array (1 ms)
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+    
--- interview-questions.md
+++ interview-questions.md
@@ -10,58 +10,70 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is `this` in JavaScript?
 
   Your answer:
+  This in Javascript refers to the object that the key is a part of. This is a keyword in JavaScript that refers to the object that it is within and it is always used on keys not values. We used the keyword this in both our classes challenges as well as our React challenges.
 
   Researched answer:
-
+  This in Javascript is a keyword that refers to the object or class that is currently calling the function. If the function that is being referenced is a regular function the this keyword is refering to the global object. If the function that is being referenced is a method in an object the this keword is refering to the object itself.
 
 
 2. What is React? Why would you use it?
 
   Your answer:
+  React is a JavaScript Library for building user interfaces and the library holds a collection of code snippets or node modules. We would use React because it handles user interactions quickly and efficiently and because it is scalable.
 
   Researched answer:
-
+  React is JavaScript library for creating user interfaces and everything in React is a component. React components are independent reusable pieces of code. Class components are similiar to functions, but are isolated and return markup through the render function. We would use React because it allows us to create large web applications that can change data without having to reload the page. We use React because it is efficient and scalable.  
 
 
 3. Which lifecycle method is required in a React class component?
 
   Your answer:
+  All React class components must have a render method. The render method is a React lifecycle method and it runs automatically whenever changes are made. We have been using this required lifecycle thorugh our React challenges.
 
   Researched answer:
+  Each component in React has a lifecycle and there are three main phases. The first phase in a lifecyle is the Mounting phase and this is when we put elements into the DOM. The render method is the only method that the component is required to have. The render method will always be called and it is the method that is used to mount the component to the DOM.The Updating phase is when a component is updated and a component is updated when there is a change in the component's state or props. The last phase in the lifecycle is when a component is removed from the DOM.
 
-
-
+Updating, and Unmounting phase
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
   Your answer:
+  JSX is a markup language that incorporates JavaScript. JSX allows us to write HTML in React and it stands for JavaScript XML. JSX uses the syntax className rather than HTML's class because class is reserved for HTML. We have been using JSX markup in React.
 
   Researched answer:
-
+  JSX stands for JavaScript syntax extension. JSX allows us to use syntax that is similar to the syntax in HTML. It is a syntax for combining JavaScript and HTML code. It allows us to write JavaScript that looks like markup and have it returned from a component.  
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
   Your answer:
+  Yarn is a package manager. It collects node modules and makes them useful by organizing them. We have used yarn when in our jest challenge as well as our weekly assessments to test our functions.
 
   Researched answer:
+  Yarn stands for Yet Another Resource Negotiator. It is a JavaScript package manager that organizes files of a React app and manages the dependencies. 
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
   Your answer:
+  An anonymous function is a function that is declared without an identifer. We recently saw how anonymous functions can be useful in our React Functional Props lecture when we needed it to stop a method from being called on automatically and used the anonymous function so that the method could be called on only when clicking the button.
 
   Researched answer:
-
+  An anonymous function does not have a name assigned to it. In regular functions we use the function keyword before the function name to define it in JavaScript. In anonymous functions only the function keyword without the function name is used. 
 
 ## Looking Ahead: Terms for Next Week
 
 1. Conditional rendering:
+  Conditional rendering in React works the same way conditions work in JavaScript. It allows us to render different elements or components based on a condition in React. We use JavaScript conditional operators to create elements that represent the current state of a component and React will update the user interface.
 
 2. Object-oriented programming:
+  Object-oriented programming (OOP) is a programming model that focuses on the objects that need to be manipulated instead of the logic required to manipulate them. An object can be defined as a data field that has unique attributes and behavior.
 
 3. Ruby:
+  Ruby is a object-oriented programming language. Ruby is a dynamic, open source, and reflective language. It has many built in methods. It is easy to use and write. 
 
 4. Ruby blocks:
+  Ruby blocks are anonymous functions that can be passed into methods. Blocks are created with do-end keywords or with curly brackets. Do-end statements are used for blocks that have multiple lines and curly brackets are used for single line blocks. Blocks can have arguments which are defined between two pipe | characters. Blocks reduce repetition and help code have less errors.
 
 5. Ruby hashes:
+  Hash in Ruby is a collection of unique keys and values. Hash is similar to an Array, except indexing is done with the keys of an object type. When iterating thorugh hashes the returning keys and values will not be in the insertion order.
